decf4118d045c16bd4629202249f1500c0530d4f
yahya <yahiamohammad740@gmail.com>
2026-01-10 17:34:03 +0200

    Stopwatch

--- DigitalClock/main.js
+++ DigitalClock/main.js
@@ -13,3 +13,52 @@ function updateClock() {
 updateClock();
 
 setInterval(updateClock, 1000);
+
+let [milliseconds, seconds, minutes] = [0, 0, 0];
+let stopwatch = document.querySelector(".stopwatch");
+let int = null;
+
+function startStopwatch() {
+  if (int !== null) {
+    clearInterval(int);
+  }
+  int = setInterval(displayTimer, 10);
+}
+
+function stopStopwatch() {
+  clearInterval(int);
+}
+
+function resetStopwatch() {
+  clearInterval(int);
+  [milliseconds, seconds, minutes] = [0, 0, 0];
+  document.getElementById("sw-min").textContent = "00";
+  document.getElementById("sw-sec").textContent = "00";
+  document.getElementById("sw-ms").textContent = "00";
+}
+
+function displayTimer() {
+  milliseconds += 10;
+
+  if (milliseconds == 1000) {
+    milliseconds = 0;
+    seconds++;
+    if (seconds == 60) {
+      seconds = 0;
+      minutes++;
+    }
+  }
+
+  let m = minutes < 10 ? "0" + minutes : minutes;
+  let s = seconds < 10 ? "0" + seconds : seconds;
+  let ms =
+    milliseconds < 10
+      ? "00" + milliseconds
+      : milliseconds < 100
+      ? "0" + milliseconds
+      : milliseconds;
+
+  document.getElementById("sw-min").textContent = m;
+  document.getElementById("sw-sec").textContent = s;
+  document.getElementById("sw-ms").textContent = String(ms).substring(0, 2);
+}
