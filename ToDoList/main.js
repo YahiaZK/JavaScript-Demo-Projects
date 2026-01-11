30bc61838ded2632a90ce026ffce8a50879383ab
yahya <yahiamohammad740@gmail.com>
2026-01-11 16:43:15 +0200

    Local Storage and Deleting todos

--- ToDoList/main.js
+++ ToDoList/main.js
@@ -1,12 +1,11 @@
-// add todo
-
 const todoForm = document.querySelector("form");
 const todoInput = document.getElementById("todo-input");
 const todoListUL = document.getElementById("todo-list");
 
-let allTodos = [];
+let allTodos = getTodos();
+updateTodoList();
 
-todoForm.addEventListener("submit", (e) => {
+todoForm.addEventListener("submit", function (e) {
   e.preventDefault();
   addTodo();
 });
@@ -14,29 +13,61 @@ todoForm.addEventListener("submit", (e) => {
 function addTodo() {
   const todoText = todoInput.value.trim();
   if (todoText.length > 0) {
-    allTodos.push(todoText);
-    insertTodo();
+    const todoObject = {
+      text: todoText,
+      completed: false,
+    };
+    allTodos.push(todoObject);
+    updateTodoList();
+    saveTodos();
     todoInput.value = "";
   }
 }
-
-function insertTodo() {
+function updateTodoList() {
   todoListUL.innerHTML = "";
   allTodos.forEach((todo, todoIndex) => {
-    const todoLI = document.createElement("li");
-    todoLI.className = "todo";
-    todoLI.innerHTML = `
+    todoItem = createTodoItem(todo, todoIndex);
+    todoListUL.append(todoItem);
+  });
+}
+function createTodoItem(todo, todoIndex) {
+  const todoId = "todo-" + todoIndex;
+  const todoLI = document.createElement("li");
+  const todoText = todo.text;
+  todoLI.className = "todo";
+  todoLI.innerHTML = `
           <input type="checkbox" id="todo-${todoIndex + 1}" />
           <label for="todo-${todoIndex + 1}" class="custom-checkbox">
             <span class="material-symbols-outlined"> check </span>
           </label>
-          <label for="todo-${todoIndex + 1}" class="todo-text"> ${todo} </label>
+          <label for="todo-${todoIndex + 1}" class="todo-text"> ${
+    todo.text
+  } </label>
           <button class="material-symbols-outlined delete-button">
             delete
-          </button>
-        `;
-
-    todoListUL.append(todoLI);
+          </button>`;
+  const deleteButton = todoLI.querySelector(".delete-button");
+  deleteButton.addEventListener("click", () => {
+    deleteTodoItem(todoIndex);
   });
+  const checkbox = todoLI.querySelector("input");
+  checkbox.addEventListener("change", () => {
+    allTodos[todoIndex].completed = checkbox.checked;
+    saveTodos();
+  });
+  checkbox.checked = todo.completed;
+  return todoLI;
 }
-
+function deleteTodoItem(todoIndex) {
+  allTodos = allTodos.filter((_, i) => i !== todoIndex);
+  saveTodos();
+  updateTodoList();
+}
+function saveTodos() {
+  const todosJson = JSON.stringify(allTodos);
+  localStorage.setItem("todos", todosJson);
+}
+function getTodos() {
+  const todos = localStorage.getItem("todos") || "[]";
+  return JSON.parse(todos);
+}
